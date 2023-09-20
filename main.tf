1a2140826432e699db4244c26c09e7261f1365ee
Daniel Fedick <daniel.fedick@hashicorp.com>
2023-09-20 06:27:58 -0400

    Move to example

--- main.tf
+++ main.tf
@@ -1,90 +1,167 @@
-locals {
-  vpc_id         = data.terraform_remote_state.vpc.outputs.vpc_id
-  public_subnets = data.terraform_remote_state.vpc.outputs.public_subnet_ids
-  cluster_name   = "hashi-k8s"
-  ami_id         = "ami-078cbc4c2d057c244"
-  tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-    "kubernetes.io/role/internal-elb"             = "1"
-    "kubernetes.io/role/nat"                      = "1"
-    "kubernetes.io/role/node"                     = "1"
-    "kubernetes.io/role/route53"                  = "1"
-    "kubernetes.io/role/vpc"                      = "1"
-    "kubernetes.io/role/elb"                      = "1"
-    "kubernetes.io/role/internal-elb"             = "1"
-    "kubernetes.io/role/nat"                      = "1"
-    "kubernetes.io/role/node"                     = "1"
-    "kubernetes.io/role/route53"                  = "1"
-    "kubernetes.io/role/vpc"                      = "1"
-  }
-  #private_key = var.private_key
-  public_key = var.public_key
+provider "aws" {
+  region = local.aws_region
+}
 
-  instance_type = "t3a.large"
-  ebs_mappings = {
-    "encrypted" = true,
-    "size"      = 30
+locals {
+  cluster_name = "hashi-k8s"
+  aws_region   = "us-east-2"
+
+  tags = {
+    "terraform" = "true",
+    "env"       = "cloud-enabled",
   }
 }
 
-# resource "local_file" "ssh_pem" {
-#   filename        = "${local.cluster_name}.pem"
-#   content         = local.private_key
-#   file_permission = "0600"
-# }
+data "aws_ami" "rhel7" {
+  most_recent = true
+  owners      = ["309956199498"] # owner is specific to aws gov cloud
 
+  filter {
+    name   = "name"
+    values = ["RHEL-7*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_ami" "rhel8" {
+  most_recent = true
+  owners      = ["309956199498"] # owner is specific to aws gov cloud
+
+  filter {
+    name   = "name"
+    values = ["RHEL-8*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_ami" "centos7" {
+  most_recent = true
+  owners      = ["679593333241"] # owner is specific to aws gov cloud
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  owners      = ["679593333241"] # owner is specific to aws gov cloud
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 8 x86_64 HVM EBS*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+# Key Pair
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "ssh_pem" {
+  filename        = "${local.cluster_name}.pem"
+  content         = tls_private_key.ssh.private_key_pem
+  file_permission = "0600"
+}
+
+#
+# Network
+#
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "rke2-${local.cluster_name}"
+  cidr = "10.88.0.0/16"
+
+  azs             = ["${local.aws_region}a", "${local.aws_region}b", "${local.aws_region}c"]
+  public_subnets  = ["10.88.1.0/24", "10.88.2.0/24", "10.88.3.0/24"]
+  private_subnets = ["10.88.101.0/24", "10.88.102.0/24", "10.88.103.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_vpn_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  # Add in required tags for proper AWS CCM integration
+  public_subnet_tags = merge({
+    "kubernetes.io/cluster/${module.rke2.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                            = "1"
+  }, local.tags)
+
+  private_subnet_tags = merge({
+    "kubernetes.io/cluster/${module.rke2.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"                   = "1"
+  }, local.tags)
+
+  tags = merge({
+    "kubernetes.io/cluster/${module.rke2.cluster_name}" = "shared"
+  }, local.tags)
+}
+
+#
+# Server
+#
 module "rke2" {
-  source = "git::https://github.com/demoland/rke2-aws-tf"
+  source = "../.."
 
-  cluster_name  = local.cluster_name
-  unique_suffix = false
-  vpc_id        = local.vpc_id
-  subnets       = local.public_subnets
+  cluster_name = local.cluster_name
+  vpc_id       = module.vpc.vpc_id
+  subnets      = module.vpc.public_subnets # Note: Public subnets used for demo purposes, this is not recommended in production
 
-  ami                   = local.ami_id
-  ssh_authorized_keys   = [local.public_key]
-  instance_type         = "t3a.large"
-  controlplane_internal = false # note this defaults to best practice of true, but is explicitly set to public for demo purposes
+  ami                   = data.aws_ami.rhel8.image_id # Note: Multi OS is primarily for example purposes
+  ssh_authorized_keys   = [tls_private_key.ssh.public_key_openssh]
+  instance_type         = "t3a.medium"
+  controlplane_internal = false # Note this defaults to best practice of true, but is explicitly set to public for demo purposes
   servers               = 1
-  # rke2_version          = "v1.26.0+rke2r1"
-  enable_ccm            = true
-  block_device_mappings = local.ebs_mappings
 
-  rke2_config = <<-eot
+  # Enable AWS Cloud Controller Manager
+  enable_ccm = true
+
+  rke2_config = <<-EOT
 node-label:
   - "name=server"
-  - "os=rhel9"
-eot
+  - "os=rhel8"
+EOT
 
   tags = local.tags
-
 }
 
 #
 # Generic agent pool
 #
-
 module "agents" {
-  source = "git::https://github.com/demoland/rke2-aws-tf//modules/agent-nodepool"
+  source = "../../modules/agent-nodepool"
 
   name    = "generic"
-  vpc_id  = local.vpc_id
-  subnets = local.public_subnets # Note: Public subnets used for demo purposes, this is not recommended in production
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets # Note: Public subnets used for demo purposes, this is not recommended in production
 
-  ami                 = local.ami_id # Note: Multi OS is primarily for example purposes
-  ssh_authorized_keys = [local.public_key]
-  spot                = false
-  # rke2_version        = "v1.26.0+rke2r1"
-
-  asg = {
-    min     = 1,
-    max     = 10,
-    desired = 3
-  }
-
-  instance_type         = local.instance_type
-  block_device_mappings = local.ebs_mappings
+  ami                 = data.aws_ami.rhel8.image_id # Note: Multi OS is primarily for example purposes
+  ssh_authorized_keys = [tls_private_key.ssh.public_key_openssh]
+  spot                = true
+  asg                 = { min : 1, max : 10, desired : 2 }
+  instance_type       = "t3a.large"
 
   # Enable AWS Cloud Controller Manager and Cluster Autoscaler
   enable_ccm        = true
@@ -93,12 +170,12 @@ module "agents" {
   rke2_config = <<-EOT
 node-label:
   - "name=generic"
-  - "os=rhel9"
+  - "os=rhel8"
 EOT
 
   cluster_data = module.rke2.cluster_data
-  tags         = local.tags
 
+  tags = local.tags
 }
 
 # For demonstration only, lock down ssh access in production
@@ -109,4 +186,19 @@ resource "aws_security_group_rule" "quickstart_ssh" {
   security_group_id = module.rke2.cluster_data.cluster_sg
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# Generic outputs as examples
+output "rke2" {
+  value = module.rke2
+}
+
+# Example method of fetching kubeconfig from state store, requires aws cli and bash locally
+resource "null_resource" "kubeconfig" {
+  depends_on = [module.rke2]
+
+  provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
+    command     = "aws s3 cp ${module.rke2.kubeconfig_path} rke2.yaml"
+  }
 }
