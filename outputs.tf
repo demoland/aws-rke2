42626f04d57b156e6dd50c67c52da9e4809024cf
Daniel Fedick <daniel.fedick@hashicorp.com>
2023-10-17 11:05:36 -0400

    Add outputs

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "cluster_data" {
+  value = module.rke2.cluster_data
+}
+
+output "cluster_name" {
+  value = module.rke2.cluster_name
+}
+
+output "cluster_sg" {
+  value = module.rke2.cluster_sg
+}
+
+output "server_url" {
+  value = module.rke2.server_url
+}
+
+output "kubeconfig_path" {
+  value = module.rke2.kubeconfig_path
+}
